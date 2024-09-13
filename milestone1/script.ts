2be7b0335a90599883dac212d51e0015b4608f16
rabiasohail098 <rabiasohail1209@gmail.com>
2024-09-13 18:28:22 +0500

    all milestones complete

---
+++ milestone1/script.ts
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementById("toggle-skills") as HTMLElement;
+const skills = document.getElementById("skills") as HTMLElement;
+toggleButton.addEventListener("click", ()=>{
+    if(skills.style.display = "block"){
+        skills.style.display = "none";
+
+    }else{
+        skills.style.display = "block"
+    }
+   
+})
